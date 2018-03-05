c475c16f08f7aa96969fb27a1ec7e013b7761908
Gatsen Tjirare <sirgatsen@gmail.com>
2018-03-05 16:38:37 +0200

    Major Cleanup, Minor Restructuring

---
+++ Assets/Scripts/DisplayAllMarkersAndLodges.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayAllMarkersAndLodges : MonoBehaviour {
+    public GameObject markers;
+    public GameObject lodges;
+    // Use this for initialization
+    void Start () {
+        lodges.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (!lodges.activeInHierarchy)
+        {
+            lodges.SetActive(true);
+            Debug.Log("Displaying now");
+        }
+        else
+        {
+            Debug.Log("Already displayed");
+            return;
+        }
+
+    }
+}
